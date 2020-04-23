33c8baa1f120822d2ca9e75eb8c050b65961a3b6
onethegarden <onethegarden@gmail.com>
2020-04-24 07:18:46 +0900

    Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {ApolloProvider} from '@apollo/react-hooks';
+import client from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root')
+);
 
