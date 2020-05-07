e9d72355b1f39ac0023ceb74e2cce15a502d147a
onethegarden <onethegarden@gmail.com>
2020-05-08 07:45:37 +0900

    local state 1

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,12 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000" //movieql실행 후 진행
+  uri: "http://localhost:4000", //movieql실행 후 진행
+  resolvers:{
+    Movie:{
+      isLiked:()=>false
+    }
+  }
 });
 
 export default client;
